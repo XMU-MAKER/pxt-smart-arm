fdc15c22bcae15d9a27e2eb1ec4c9deef4e1ff71
windwood <windwood@gmail.com>
2019-03-16 16:07:07 +0800

    Testing module

--- main.ts
+++ main.ts
@@ -5,3 +5,12 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+namespace smart-arm {
+    function signal_dht11(pin: DigitalPin): void {
+        pins.digitalWritePin(pin, 0);
+        basic.pause(18);
+        let i = pins.digitalReadPin(pin);
+        pins.setPull(pin, PinPullMode.PullUp);
+    }
+}
