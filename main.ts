1d83ec9415f93a2660051c4800c23a99c984e746
windwood <windwood@gmail.com>
2019-03-16 16:23:13 +0800

    change color and name

--- main.ts
+++ main.ts
@@ -1,17 +1,5 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
-
 //% weight=0 color=#1C809D icon="\uf2db" block="sensorssss"
 namespace smart-arm {
-    function signal_dht11(pin: DigitalPin): void {
-        pins.digitalWritePin(pin, 0);
-        basic.pause(18);
-        let i = pins.digitalReadPin(pin);
-        pins.setPull(pin, PinPullMode.PullUp);
+    function signal_dht11(): void {
     }
 }
