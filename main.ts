6718b8ded85be4bbf9a4ea950f45d2de5083bf65
windwood <windwood@gmail.com>
2019-03-16 16:19:32 +0800

    change color and name

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ basic.showLeds(`
     . # # # .
     `);
 
+//% weight=0 color=#1C809D icon="\uf2db" block="sensorssss"
 namespace smart-arm {
     function signal_dht11(pin: DigitalPin): void {
         pins.digitalWritePin(pin, 0);
